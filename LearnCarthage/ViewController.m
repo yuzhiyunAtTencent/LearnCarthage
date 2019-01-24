5bbcc2a555015a98226cbfe3c2324925b8b604db
zhiyunyu <zhiyunyu@tencent.com>
2019-01-24 14:54:44 +0800

    集成 SDWebImage

--- LearnCarthage/ViewController.m
+++ LearnCarthage/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <SDWebImage/UIImageView+WebCache.h>
 
 @interface ViewController ()
+
+@property(nonatomic, strong) UIImageView *imgView;
 
 @end
 
@@ -17,6 +20,10 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    self.imgView = [[UIImageView alloc] initWithFrame:CGRectMake(10, 64, 300, 400)];
+    [self.view addSubview:self.imgView];
+    
+    [self.imgView sd_setImageWithURL:[[NSURL alloc] initWithString:@"https://images.unsplash.com/photo-1548110984-4ba6a15f0bcc?ixlib=rb-1.2.1&auto=format&fit=crop&w=1600&q=80"]];
 }
 
 
